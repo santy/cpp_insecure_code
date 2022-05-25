b109b39b1b726e09a564171a89e693c7af2d51b1
Santy <santy@cierco.es>
2022-05-25 12:24:25 +0200

    Comienzo de proyecto

---
+++ Script1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int  main(int argc, char **argv){
+    char secreto[10] = "secreto";
+    char var[10];
+    strcpy(var, argv[1]);
+    printf ("Var 1: %s\n", var);
+    printf ("var 2: %s\n", secreto);
+    return;
+}
